f76b4080201491df5ff753ec3dbe6de0f998a729
Bori <boriakinola01@gmail.com>
2023-11-06 16:47:06 +0000

    Initial Commit

---
+++ MetUCD/MetUCDApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetUCDApp.swift
+//  MetUCD
+//
+//  Created by Bori Akinola on 06/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct MetUCDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
