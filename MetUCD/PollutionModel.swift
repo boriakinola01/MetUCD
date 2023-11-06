9a0afab51fd520f99907d0237d3d9d599a8ac23e
Bori <boriakinola01@gmail.com>
2023-11-06 17:09:14 +0000

    implemented pollution model

--- MetUCD/PollutionModel.swift
+++ MetUCD/PollutionModel.swift
@@ -6,3 +6,30 @@
 //
 
 import Foundation
+struct PollutionModel: Codable {
+    // Fields gotten from the OpenWeatherMap Air Quality API
+    
+    // components.co
+    var co: Double
+    
+    // components.no
+    var no: Double
+    
+    // components.no2
+    var no2: Double
+    
+    // components.o3
+    var o3: Double
+    
+    // components.pm10
+    var pm10: Double
+    
+    // components.pm2_5
+    var pm2_5: Double
+    
+    // components.so2
+    var so2: Double
+    
+    // components.nh3
+    var nh3: Double
+}
--- MetUCD/WeatherModel.swift
+++ MetUCD/WeatherModel.swift
@@ -6,3 +6,33 @@
 //
 
 import Foundation
+
+struct WeatherModel: Codable {
+    
+    // All fields gotten from the OpenWeather API
+    
+    struct Wind: Codable {
+        // current.wind_speed
+        var speed: Double
+        // current.wind_deg
+        var direction: Int
+    }
+    
+    // current.temp
+    var temp: Double
+    
+    // current.feels_like
+    var feelsLike: Double
+    
+    // current.clouds
+    var cloudCoverage: Double
+    
+    // Wind information
+    var wind: Wind
+    
+    // currrent.pressure
+    var pressure: Double
+    
+    // current.humidity
+    var humidity: Double
+}
