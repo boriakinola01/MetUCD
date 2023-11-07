4385a8f6a72445055e8e10ff14763b0e0f341b6d
Bori <boriakinola01@gmail.com>
2023-11-07 11:03:59 +0000

    updated models, added geocode model

---
+++ MetUCD/GeocodeModel.swift
@@ -0,0 +1,17 @@
+//
+//  GeocodeModel.swift
+//  MetUCD
+//
+//  Created by Bori Akinola on 07/11/2023.
+//
+
+import Foundation
+
+struct GeocodeModel : Codable {
+    
+    
+    var longitude: Double
+    
+    var latitude: Double
+    
+}
--- MetUCD/PollutionModel.swift
+++ MetUCD/PollutionModel.swift
@@ -6,30 +6,32 @@
 //
 
 import Foundation
+
 struct PollutionModel: Codable {
     // Fields gotten from the OpenWeatherMap Air Quality API
+    // https://api.openweathermap.org/data/2.5/air_pollution
     
-    // components.co
+    // list[0].components.co
     var co: Double
     
-    // components.no
+    // list[0].components.no
     var no: Double
     
-    // components.no2
+    // list[0].components.no2
     var no2: Double
     
-    // components.o3
+    // list[0].components.o3
     var o3: Double
     
-    // components.pm10
+    // list[0].components.pm10
     var pm10: Double
     
-    // components.pm2_5
+    // list[0].components.pm2_5
     var pm2_5: Double
     
-    // components.so2
+    // list[0].components.so2
     var so2: Double
     
-    // components.nh3
+    // list[0].components.nh3
     var nh3: Double
 }
